6cd7a88707b50cb681d84c56c5dca2df3951192e
gedvsfm <609788083@qq.com>
2018-08-08 14:38:53 +0800

    util

--- WebServer/Util.cpp
+++ WebServer/Util.cpp
@@ -181,7 +181,162 @@ ssize_t writen(int fd, std::string &sbuff)
         sbuff = sbuff.substr(writeSum);
     return writeSum;
 }
+int read_timeout(int fd, unsigned int wait_seconds)
+{
+	int ret = 0;
+	if (wait_seconds > 0) {
+		fd_set read_fdset;
+		struct timeval timeout;
 
+		FD_ZERO(&read_fdset);
+		FD_SET(fd, &read_fdset);
+
+		timeout.tv_sec = wait_seconds;
+		timeout.tv_usec = 0;
+		do {
+			ret = select(fd + 1, &read_fdset, NULL, NULL, &timeout);
+		} while (ret < 0 && errno == EINTR);
+
+		if (ret == 0) {
+			ret = -1;
+			errno = ETIMEDOUT;
+		}
+		else if (ret == 1)
+			ret = 0;
+	}
+
+	return ret;
+}
+
+/**
+ * write_timeout - 读超时检测函数，不含写操作
+ * @fd: 文件描述符
+ * @wait_seconds: 等待超时秒数，如果为0表示不检测超时
+ * 成功（未超时）返回0，失败返回-1，超时返回-1并且errno = ETIMEDOUT
+ */
+int write_timeout(int fd, unsigned int wait_seconds)
+{
+	int ret = 0;
+	if (wait_seconds > 0) {
+		fd_set write_fdset;
+		struct timeval timeout;
+
+		FD_ZERO(&write_fdset);
+		FD_SET(fd, &write_fdset);
+
+		timeout.tv_sec = wait_seconds;
+		timeout.tv_usec = 0;
+		do {
+			ret = select(fd + 1, NULL, NULL, &write_fdset, &timeout);
+		} while (ret < 0 && errno == EINTR);
+
+		if (ret == 0) {
+			ret = -1;
+			errno = ETIMEDOUT;
+		}
+		else if (ret == 1)
+			ret = 0;
+	}
+
+	return ret;
+}
+
+/**
+ * accept_timeout - 带超时的accept
+ * @fd: 套接字
+ * @addr: 输出参数，返回对方地址
+ * @wait_seconds: 等待超时秒数，如果为0表示正常模式
+ * 成功（未超时）返回已连接套接字，超时返回-1并且errno = ETIMEDOUT
+ */
+int accept_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
+{
+	int ret;
+	socklen_t addrlen = sizeof(struct sockaddr_in);
+
+	if (wait_seconds > 0) {
+		fd_set accept_fdset;
+		struct timeval timeout;
+		FD_ZERO(&accept_fdset);
+		FD_SET(fd, &accept_fdset);
+		timeout.tv_sec = wait_seconds;
+		timeout.tv_usec = 0;
+		do {
+			ret = select(fd + 1, &accept_fdset, NULL, NULL, &timeout);
+		} while (ret < 0 && errno == EINTR);
+        if (ret == -1) {
+			return -1;
+        }
+		else if (ret == 0) {
+			errno = ETIMEDOUT;
+			return -1;
+		}
+	}
+
+	if (addr != NULL)
+		ret = accept(fd, (struct sockaddr*)addr, &addrlen);
+	else
+		ret = accept(fd, NULL, NULL);
+
+	return ret;
+}
+
+/**
+ * connect_timeout - connect
+ * @fd: 套接字
+ * @addr: 要连接的对方地址
+ * @wait_seconds: 等待超时秒数，如果为0表示正常模式
+ * 成功（未超时）返回0，失败返回-1，超时返回-1并且errno = ETIMEDOUT
+ */
+int connect_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
+{
+	int ret;
+	socklen_t addrlen = sizeof(struct sockaddr_in);
+
+	if (wait_seconds > 0)
+		activate_nonblock(fd);
+
+	ret = connect(fd, (struct sockaddr*)addr, addrlen);
+	if (ret < 0 && errno == EINPROGRESS) {
+		fd_set connect_fdset;
+		struct timeval timeout;
+		FD_ZERO(&connect_fdset);
+		FD_SET(fd, &connect_fdset);
+		timeout.tv_sec = wait_seconds;
+		timeout.tv_usec = 0;
+		do {
+			ret = select(fd + 1, NULL, &connect_fdset, NULL, &timeout);
+		} while (ret < 0 && errno == EINTR);
+		if (ret == 0) {
+			ret = -1;
+			errno = ETIMEDOUT;
+		}
+		else if (ret < 0)
+			return -1;
+		else if (ret == 1) {
+			//printf("BBBBB\n");
+			/* ret返回为1，可能有两种情况，一种是连接建立成功，一种是套接字产生错误，*/
+			/* 此时错误信息不会保存至errno变量中，因此，需要调用getsockopt来获取。 */
+			int err;
+			socklen_t socklen = sizeof(err);
+			int sockoptret = getsockopt(fd, SOL_SOCKET, SO_ERROR, &err, &socklen);
+			if (sockoptret == -1) {
+				return -1;
+			}
+			if (err == 0) {
+				//printf("DDDDDDD\n");
+				ret = 0;
+			} else {
+				//printf("CCCCCC\n");
+				errno = err;
+				ret = -1;
+			}
+		}
+	}
+	if (wait_seconds > 0) {
+		deactivate_nonblock(fd);
+	}
+	return ret;
+}
 void handle_for_sigpipe()
 {
     struct sigaction sa;
--- WebServer/Util.h
+++ WebServer/Util.h
@@ -9,6 +9,12 @@ ssize_t readn(int fd, std::string &inBuffer, bool &zero);
 ssize_t readn(int fd, std::string &inBuffer);
 ssize_t writen(int fd, void *buff, size_t n);
 ssize_t writen(int fd, std::string &sbuff);
+
+int read_timeout(int fd, unsigned int wait_seconds);
+int write_timeout(int fd, unsigned int wait_seconds);
+int accept_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds);
+int connect_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds);
+
 void handle_for_sigpipe();
 int setSocketNonBlocking(int fd);
 void setSocketNodelay(int fd);
